f7426ee8249ddffe2ef47769dfa3926f23b5cef8
Carlos-Rojas <carlosluzumbero@gmail.com>
2026-05-14 01:36:07 -0700

    no blocks

--- src/App.jsx
+++ src/App.jsx
@@ -273,6 +273,7 @@ export default function App() {
               "JavaScript",
               "Python",
               "C#",
+              "C++",
               "Go",
               "React",
               "Next.js",
